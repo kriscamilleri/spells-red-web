d3d179ee1f479c97cb013676846491ac0600fc3a
Kris Camilleri <kris.camilleri@gmail.com>
2021-12-28 17:09:51 +0100

    basic card layout

--- src/components/SpellCard.vue
+++ src/components/SpellCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border p-5 max-w-sm">
+    <div class="border p-5 max-w-sm m-2">
         <slot></slot>
     </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <SlideOverFrame>
-    <SpellCard v-for="number in repetitions" :key="number" class="m-2">
-      <div>Content</div>
-      <router-link to="/Spell/1234" class="underline">Read me!</router-link>
-    </SpellCard>
+    <div class="flex flex-wrap justify-center">
+      <SpellCard v-for="number in repetitions" :key="number" class="max-w-[24rem] min-w-[24rem]">
+        <div>Content</div>
+        <router-link to="/Spell/1234" class="underline">Read me!</router-link>
+      </SpellCard>
+    </div>
   </SlideOverFrame>
 </template>
 <style>
